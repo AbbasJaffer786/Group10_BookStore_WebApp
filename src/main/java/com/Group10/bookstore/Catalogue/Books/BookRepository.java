323d6de227334527c6e98cf51404f81d668b6d14
jakimov99 <81499315+jakimov99@users.noreply.github.com>
2021-12-05 23:01:01 -0500

    Add files via upload
    Added 3 out of 4 features and changed the array list of Books to include different names and different genres
    - Retrieve list of books by genre (finished) retrieved books by genre either by fantasy or comedy (more genres can be implemented)
    - Retrieve List of Top sellers (finished) retrieved books by most seller count by descending order starting out with the book with most sales
    - Retrieve list of books for a particular rating (finished) sort out rating of books by descending order starting out with highest rating going down
    - I was not able to implement the last one

--- src/main/java/com/Group10/bookstore/Catalogue/Books/BookRepository.java
+++ src/main/java/com/Group10/bookstore/Catalogue/Books/BookRepository.java
@@ -2,14 +2,17 @@ package com.Group10.bookstore.Catalogue.Books;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer>{
-	List<Book> findTop10ByOrderBySalesCNTDesc();
 	Book findByIsbn(String isbn);
 	List<Book> findByAuthor(String Author);
 	List<Book> findByGenre(String genre);
+	List<Book> findTop10ByOrderBySalesCNTDesc();
+	List<Book> findTop10ByOrderByRatingCNTDesc();
+
 }
